b1635791c65f3fce66c3a3ab9a2d5202f0b30300
afnanaffandi <120000628+afnanaffandi@users.noreply.github.com>
2023-02-02 14:00:11 +0800

    Add files via upload

---
+++ New_Case_Study.sql
@@ -0,0 +1,174 @@
+select * 
+from [dbo].['Q2 data$']
+
+--Find The avg for built up price psf,size built up and valuation for Flat Jalan Aman
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20161 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20162
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20163
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20164
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20171 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20172
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20173
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20174
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20181 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20182
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20183
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20184
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20191 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'FLAT JALAN AMAN' and QUARTER = 20192
+
+
+--Find The avg for built up price psf,size built up and valuation for Astra Suites
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20161 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20162
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20163
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20164
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20171 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20172
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20173
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20174
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20181 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20182
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20183
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20184
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20191 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20192
+
+--Find The avg for built up price psf,size built up and valuation for Lily Point
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20161 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20162
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20163
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20164
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME = 'LILY POINT' and QUARTER = 20171 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20172
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20173
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20174
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20181 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'ASTRA SUITES' and QUARTER = 20182
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20183
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20184
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20191 
+
+select Avg([Built up price psf]) AS Avg_Built_Up_Price_Psf,Avg([Size built up]) as Avg_Size_Built_Up ,AVG([Valuation price]) as Avg_Valuation_Price
+from [dbo].['Q2 data$']
+where NAME =  'LILY POINT' and QUARTER = 20192
